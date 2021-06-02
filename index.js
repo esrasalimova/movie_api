e73ea27e4d90b8fd62b2dece59d6583d99d72f3b
esrasalimova <82524635+esrasalimova@users.noreply.github.com>
2021-06-02 13:38:53 +0200

    added documentation in index.html

--- index.js
+++ index.js
@@ -49,8 +49,45 @@ app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
+app.get('/movies', (req, res) => {
+   res.send('Successful GET request returning data on all movies');
+ });
 
+ app.get('/movies/[movie title]', (req, res) => {
+   res.send('Successful GET request returning data about specific movie');
+ });
 
+ app.get('movies/[genre]', (req, res) => {
+    res.send('Successful GET request returning data about list of movies by genre');
+  });
+
+  app.get('/directors/[directors info]', (req, res) => {
+   res.send('Successful GET request returning data information about specific director');
+ });
+
+ app.post('/user', (req, res) => {
+   res.send('Successful POST request returning data that user to register a new account');
+ });
+
+ app.get('/users/[username]', (req, res) => {
+   res.send('Successful GET request returning data that User can view their account details');
+ });
+
+ app.remove('/users/[username]', (req, res) => {
+   res.send('Successful REMOVE request returning data Allows a user unregister and delete their account');
+ });
+
+ app.put('/users/[user-name]', (req, res) => {
+   res.send('Successful PUT request returning data that Update a users information');
+ });
+
+ app.post('/users/[username]/Movies/[Movie ID]', (req, res) => {
+   res.send('Successful POST request returning data that Adds a movie to the users favorites list');
+ });
+
+ app.post('/users/[username]/Movies/remove/[MovieID]', (req, res) => {
+   res.send('Successful POST request returning data that Removes a movie from the users favorites list');
+ });
 
 // listen for requests
 app.listen(8080, () =>{
