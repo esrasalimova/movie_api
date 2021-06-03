100bf3798067c4c83beedf8d5a65f00852ce2019
esrasalimova <82524635+esrasalimova@users.noreply.github.com>
2021-06-03 19:21:53 +0200

    managed endpoints

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const morgan = require('morgan');
 app.use(morgan('common'));
 
-const uuid = require('uuid');
 
 const bodyParser = require('body-parser');
 
@@ -50,80 +49,23 @@ app.get('/secreturl', (req, res) => {
 
 
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.send('this gets list of all available movies with image and title');
 });
 
 
  app.get('/movies/:title', (req, res) => {
-   res.json(movies.find((title) =>
-   { return movie.title ===req.params.title }));
+   res.send('this gets available data about specific movie' + req.params.name);
  });
 
  app.get('movies/:genre', (req, res) => {
-    res.json(movies.find((genre) =>
-    {return movies.genre === req.params.genre }));
-  });
+   res.send('this gets a list of movies by genre' + req.params.genre);
+ });
 
   app.get('/directors/:info', (req, res) => {
-   res.json(directors.find((directors) =>
-   { return directors.info === req.params.info}));
- });
+    res.send('this gets a information about specific director' + req.params.info);
+  });
 
- app.post('/user', (req, res) => {
-   let user = req.body;
-   if (!newUser.name) {
-     const message = 'Missing name in request body';
-     res.status(400).send(message);
-   } else {
-     newUser.id = uuid.v4();
-     user.push(newUser);
-     res.status(201).send(newUser);
-   }
- });
 
- app.get('/users/:username', (req, res) => {
-   res.json(users.find((user) =>
-   { return user.name === req.params.username}));
- });
-
- app.delete('/users/:username', (req, res) => {
-   let users = users.find((user) => { return user.name ===req.params.name});
-   if (user) {
-     users = users.filter((obj) => { return obj.name !==req.params.name});
-     res.status(201).send('User' + req.params.name + ' was deleted.');
-     }
- });
-
- app.put('/users/:name', (req, res) => {
-   let user = users.find((user) => { return user.name ===req.params.name});
-
-   if (user) {
-     user.name[req.params.name] =(req.params.name);
-     res.status(201).send('User' + req.params.name + 'information updated');
-   } else {
-     res.status(404).send('User with name' + req.params.name + 'was not found.');
-   }
- });
-
- app.put('/users/:username/:movies/:movieid', (req, res) => {
-   let user = users.find((user) => { return user.name ===req.params.name});
-
-   if (user) {
-     user.movies[req.params.movie] = parseInt(req.params.movieid);
-     res.status(201).send('User' + req.params.username + 'added to favorites' + req.params.movie + req.params.movieid);
-   } else {
-     res.status(404).send('User with the name ' + req.params.name + 'was not found.');
-   }
- });
-
- app.delete('/users/:username/:movies/:remove/:movieid', (req, res) => {
-   let user = users.find((user) => {return movie.name === req.params.name });
-
-   if (user) {
-     users = users.filter((obj) => {return obj.name !== req.params.name});
-     res.status(201).send('movie was removed.');
-   }
- });
 
 // listen for requests
 app.listen(8080, () =>{
