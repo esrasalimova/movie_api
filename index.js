f9b3bebb2c9cd1bb15d293186fd4a90de8c7001e
esrasalimova <82524635+esrasalimova@users.noreply.github.com>
2021-06-17 15:10:28 +0200

    changed mongodb uri to env

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Users = Models.User;
 const Directors = Models.Director;
 const Genres = Models.Genre;
 
-mongoose.connect('mongodb+srv://purple-cat02:thisispurplecat02@cluster0.pp3fb.mongodb.net/MyFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
